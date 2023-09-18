a54ec539aebcc7d05f999ca9fdaa5904e45ca17b
Rodrigo <rodrigo.vale.st@gmail.com>
2023-09-18 10:57:33 -0300

    [mod] fixing homepage bug

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,13 @@
 import Generator from "./pages/Generator";
 import Decoder from "./pages/Decoder";
 import Error from "./pages/Error";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/generator" />} />
         <Route path="/generator" element={<Generator />} />
         <Route path="/decoder" element={<Decoder />} />
         <Route path="*" element={<Error />} />
